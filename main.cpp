0dd85d28efa97d4f356e5946d9d2639b907f6339
coffeeman1229 <coffeeman1229@naver.com>
2022-02-08 10:21:41 +0900

    220208

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "SDL.h"
+using namespace std;
+
+//링커 - 입력
+#pragma comment (lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+
+
+
+int SDL_main(int argc, char* argv[])
+{
+
+
+
+	return 0;
+}
